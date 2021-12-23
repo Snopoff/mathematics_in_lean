c82a407cddfbe42ff4190ffe5d8afc92e30c1d3b
Snopoff <snopoffp@gmail.com>
2021-12-24 01:23:37 +0300

    first example solved

--- examples/basics/unnamed_115.lean
+++ examples/basics/unnamed_115.lean
@@ -3,11 +3,15 @@ import data.real.basic
 -- BEGIN
 example (a b c : ℝ) : (c * b) * a = b * (a * c) :=
 begin
-  sorry
+  rw mul_comm c b,
+  rw mul_comm a c,
+  rw mul_assoc,
 end
 
 example (a b c : ℝ) : a * (b * c) = b * (a * c) :=
 begin
-  sorry
+  rw ← mul_assoc,
+  rw mul_comm a b,
+  rw mul_assoc,
 end
 -- END
